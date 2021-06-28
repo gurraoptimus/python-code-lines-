1f3d0e6aa8f8901b8d490c9f39e3450c58de3a59
gurraoptimus <ranibowmonstergurra1+psn@gmail.com>
2021-06-28 21:19:07 +0200

    readme.m

---
+++ README.m
@@ -0,0 +1,9 @@
+ //=========================================
+# {G}
+# {gurraoptimus.se}
+# python-code-lines.
+# this is my python-code-list. 
+ 
+  Copyright © gurraoptimus.se 2021. All rights reserved
+
+ //=========================================
