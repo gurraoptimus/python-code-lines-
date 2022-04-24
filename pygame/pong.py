1bc77a9989d31c5fe4017a1057c256dab556fc7f
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2022-04-24 02:18:26 +0200

    Pong

--- pygame/pong.py
+++ pygame/pong.py
@@ -4,11 +4,19 @@ WIDTH, HIGHT = 700, 500
 WID = pygame.display.set_mode((WIDTH, HIGHT))
 pygame.display.set_caption("Pong")
 
+
+
+FPS = 60
+
+def
+
 def main():
     run = True
     clock = pygame.time.Clock()
 
-    while run:for event in pygame.event.get():
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
         if event.tye == pygame.QUIT:
             run = False
             break
