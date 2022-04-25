fd0e07c1730b7f79e6365a8e3378ea405a239118
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2022-04-25 22:24:18 +0200

    pong

--- pygame/pong.py
+++ pygame/pong.py
@@ -26,7 +26,7 @@ def draw(self,win):
     (self.COLOR(self.x self.y, self.width, self.height))
 
 
-def draw(win):
+def draw(win, paddle):
     win.fill(BLACK)
     pygame.display.update()
 
@@ -35,10 +35,14 @@ def main():
     run = True
     clock = pygame.time.Clock()
 
-    left_paddle = Paddle(10, HIGHT//2 - PADDLE_HIGHT//2)
+    left_paddle = Paddle(10, HIGHT//2 - PADDLE_HIGHT//2, PADDLE_WIDTH, PADDLE_HIGHT)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HIGHT//2 - PADDLE_HIGHT//2, PADDLE_WIDTH, PADDLE_HIGHT)
+
 
     while run:
         clock.tick(FPS)
+        draw(WIN, [left_paddle right_paddle])
+        
         for event in pygame.event.get():
             if event.tye == pygame.QUIT:
                 run = False
