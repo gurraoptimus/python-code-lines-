b32f25e57562cb2339b3a5f7cdbc702158b79251
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2022-08-07 21:21:33 +0200

    pong

--- pygame/pong.py
+++ pygame/pong.py
@@ -1,58 +1,115 @@
 import pygame
 
+
+
 WIDTH, HIGHT = 700, 500
+
 WIN = pygame.display.set_mode((WIDTH, HIGHT))
+
 pygame.display.set_caption("Pong")
+
+
+
 
 
 FPS = 60
 
+
+
 WHITE = (255, 255, 255)
+
 BLACK = (0, 0, 0)
+
+
 
 PADDLE_WIDTH, PADDLE_HIGHT  = 20, 100
 
+
+
 class Paddle:
+
     COLOR = WHITE
 
-    def _init_(self, x, y, width, height):
+
+
+    def __init__(self, x, y, width, height):
+
         self.x = x
+
         self.y = y
+
         self.width = width
+
         self.height = height
 
-def draw(self, win):
-    pygame.draw.rect(
-    win,self.COLOR)(self.x, self.y, self.width, self.height)
+
+
+    def draw(self, win):
+
+        pygame.draw.rect(win,self.COLOR,(self.x, self.y, self.width, self.height))
+
+
+
 
 
 def draw(win, paddle):
-    win.fill(BLACK)
+
     paddle.draw(win)
 
-    pygame.display.update()
+
+
+
+
 
 
 def main():
+
     run = True
+
     clock = pygame.time.Clock()
 
+
+
     left_paddle = Paddle(10, HIGHT//2 - PADDLE_HIGHT//2, PADDLE_WIDTH, PADDLE_HIGHT)
+
     right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HIGHT//2 - PADDLE_HIGHT//2, PADDLE_WIDTH, PADDLE_HIGHT)
 
 
+
+
+
     while run:
+
+        WIN.fill(BLACK)
+
         clock.tick(FPS)
-        draw(WIN, left_paddle) [right_paddle]
+
+        draw(WIN, left_paddle)
+
+        draw(WIN, right_paddle)
+
+        pygame.display.update()
+
         
-        for Event in pygame.Event.get():
+
+        for Event in pygame.event.get():
+
             if Event.type == pygame.QUIT:
+
                 run = False
+
                 break
+
+
+
 
 
     pygame.quit()
 
 
-if __name__ == "_main_":
-    main()
+
+
+
+
+
+main()
