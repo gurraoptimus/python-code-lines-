a55f22a17ab6c2375f0d9bec9b8275649eee9d6d
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2022-11-06 04:24:05 +0100

    PYgame

--- pygame/pong.py
+++ pygame/pong.py
@@ -78,9 +78,9 @@ def main():
 
         clock.tick(FPS)
 
-        draw_window(WIN, left_paddle)
+        draw_window (WIN, left_paddle)
 
-        draw_window(WIN, right_paddle)
+        draw_window (WIN, right_paddle)
 
         pygame.display.update()
 
