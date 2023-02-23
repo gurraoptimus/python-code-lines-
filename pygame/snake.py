5bf9f055965baf12e4f5be9f267da282e3922b65
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-02-23 15:47:54 +0100

    PYgame

--- pygame/snake.py
+++ pygame/snake.py
@@ -9,7 +9,7 @@ snake = pg.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE - 2])
 snake.center = get_random_position()
 length = 1
 segments = [snake.copy()]
-snake_dir = (0,0)
+snake_dir = (0, 0)
 screen = pg.display.set_mode([WINDOW] * 2)
 Clock = pg.time.Clock()
 
