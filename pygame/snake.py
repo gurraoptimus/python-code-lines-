dbf22ece75ae0c4237439889dda68752343ac116
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-02-23 14:28:36 +0100

    PYgame

--- pygame/snake.py
+++ pygame/snake.py
@@ -4,13 +4,14 @@ from random import randrange
 WINDOW = 1000
 TILE_SIZE = 50
 RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
-get_random_position = lambda: [r
+get_random_position = lambda: [randrange(*RANGE), randrange(*RANGE)]
+
 screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()
 
 while True:
     for event in pg.event.get():
-        if event.type == pg.QUIT
+        if event.type == pg.QUIT:
             exit()
     screen.fill("black")
     pg.display.fill()
