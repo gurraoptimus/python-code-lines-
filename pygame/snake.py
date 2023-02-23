f2043ecf5e74b75041acbd68f34b9b41eb743253
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-02-23 14:22:16 +0100

    PYgame

--- pygame/snake.py
+++ pygame/snake.py
@@ -4,7 +4,7 @@ from random import randrange
 WINDOW = 1000
 TILE_SIZE = 50
 RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
-
+get_random_position = lambda: [r
 screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()
 
