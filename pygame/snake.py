876ecb4b011738b71cddcb13c5ac896b01fe001f
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-02-23 14:11:03 +0100

    PYgame

--- pygame/snake.py
+++ pygame/snake.py
@@ -2,8 +2,14 @@ import pygame as pg
 from random import randrange
 
 WINDOW = 1000
+TI
 screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()
 
 while True:
-    for event if
+    for event in pg.event.get():
+        if event.type == pg.QUIT
+            exit()
+    screen.fill("black")
+    pg.display.fill()
+    Clock.tick(60)
