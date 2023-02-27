7b44e180d0837a87d334a6789f94809305a158a0
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-02-27 14:13:37 +0100

    PYgame

--- pygame/snake.py
+++ pygame/snake.py
@@ -31,7 +31,7 @@ while True:
                 snake_dir = (TILE_SIZE, 0)
     screen.fill("black")
     #check Borders and selfieating
-    self_eating = pg.Rect.collidelist(snake, segments[:-1] != -1
+    self_eating = pg.Rect.collidelist(snake, segments[:-1]) != -1
     if snake.left < 0 or snake.right > WINDOW or snake.top < 0 or snake.bottom > WINDOW or selfeating:
          snake.center, food.center = get_random_position(), get_random_position()
     length, snake_dir = 1, (0, 0)
