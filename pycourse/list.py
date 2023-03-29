4de3d047e5cd2058700efefb2b854dffadde5d30
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-03-29 11:24:44 +0200

    pycorse

--- pycourse/list.py
+++ pycourse/list.py
@@ -1,6 +1,12 @@
+#print("[", end="")
+#k= 0
+#while k <6:
+   # print(f"{k:2}",end="")
+ #   k = k + 2
+#print(" ]")
+
+
 print("[", end="")
-k= 0
-while k <6:
+for k  in range(0,7,1):
     print(f"{k:2}",end="")
-    k = k + 2
 print(" ]")
