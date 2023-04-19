502944915797f1dd87a81842e9e1b701635c3ad4
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-04-19 11:25:31 +0200

    pycourse

--- pycourse/datetineyear.py
+++ pycourse/datetineyear.py
@@ -3,10 +3,14 @@
 # ar = 2023 -b
 # print(ar)
 
+# import datetime
+# n = input("vad är ditt namn:")
+# f = int(input("när är du född:"))
+# ct = datetime.datetime.now()
+# ar = ct.year-f
+# for i in range(100):
+#    print(i+1,n,ar,ct.year)
+
 import datetime
-n = input("vad är ditt namn:")
-f = int(input("när är du född:"))
-ct = datetime.datetime.now()
-ar = ct.year-f
-for i in range(100):
-   print(i+1,n,ar,ct.year)
+dt=datetime.datetime.now()
+n = input("vilken dag och mån & år dth:")
